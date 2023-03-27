95cce1dffaf376ddb1fe5b9ec71671c7bbe8a391
scn-tech <scnaikwadi2@gmail.com>
2023-03-27 09:44:01 +0530

    Array In Java
    Array In Java

---
+++ code/corejava/basic/Array1D.java
@@ -0,0 +1,19 @@
+public class Array1D
+{
+	public static void main(String arr[])
+	{
+		/*
+		double percent[];
+		percent = new double[5];
+		percent[0] = 77.88;
+		percent[1] = 89.91;
+		*/
+
+		double percent[] = {77.88, 89.91, 67.76, 91.12, 78.32};
+
+		for(int i = 0; i<=4; i++ ) {
+			System.out.println(percent[i]);
+		}
+
+	}
+}
