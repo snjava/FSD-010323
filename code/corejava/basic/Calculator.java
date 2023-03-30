8b6c177538cf79fcf4e20f984f7741e654a8f7b7
scn-tech <scnaikwadi2@gmail.com>
2023-03-30 10:37:36 +0530

    Class Object and methods
    Class Object and methods

---
+++ code/corejava/basic/Calculator.java
@@ -0,0 +1,23 @@
+public class Calculator
+{
+	public static void main(String ar[])
+	{
+		System.out.println("Main Methods Started...");
+		Calculator cal; // reference Varaible
+		cal = new Calculator(); // Object
+		int result = cal.findAge(2002); // calling method using object
+		System.out.println(result);
+		System.out.println("Main Methods Ends...");
+	}
+	
+	// get the birth year, calculate the age and return the age
+	public void findAge(int byear)
+	{
+		System.out.println("Calculating the Age...");
+		int cyear = 2023;
+		int age = cyear - byear;
+		return age;
+	} 
+
+	
+}
