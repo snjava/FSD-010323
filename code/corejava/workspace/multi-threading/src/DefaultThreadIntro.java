87f3040b4cd4d188cacc3fccfd5ae546a3abda76
snjava <scnaikwadi2@gmail.com>
2023-04-24 10:38:59 +0530

    Thread Intro
    Thread Intro

---
+++ code/corejava/workspace/multi-threading/src/DefaultThreadIntro.java
@@ -0,0 +1,8 @@
+
+public class DefaultThreadIntro {
+	public static void main(String[] args) {
+		System.out.println("main Thread started...");
+		System.out.println(Thread.currentThread());
+		System.out.println("main Thread end...");
+	}
+}
