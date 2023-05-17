19a962f0dd9d6b7728f2b9c04cd819e348218474
snjava <scnaikwadi2@gmail.com>
2023-05-17 09:42:02 +0530

    JDBC Intro
    JDBC Intro

--- code/corejava/workspace/JDBC/src/DbConnectionCheck.java
+++ code/corejava/workspace/JDBC/src/DbConnectionCheck.java
@@ -5,9 +5,10 @@ public class DbConnectionCheck {
 	public static void main(String[] args) {
 		try {
 			
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/fsd01march","root","root");
+			Class.forName("com.mysql.cj.jdbc.Driver"); // 1. Register Driver
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/fsd01march","root","root"); // 2. Create Connection
 			System.out.println("Connection Successful...");
+			con.close(); // 5. Close Connection
 			
 		} catch(Exception e) {
 			e.printStackTrace();
